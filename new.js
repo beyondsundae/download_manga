fbf9fe7357e326dd5993984c07c84d24bef3f800
DESKTOP-TT11N69\p0000 <p00000rice@gmail.com>
2021-10-29 03:41:34 +0700

    initial

---
+++ new.js
@@ -0,0 +1,11 @@
+// process.argv.forEach(function (val, index, array) {
+//     // 1: C:\Users\p0000\Desktop\New folder (2)\new.js // current folder
+//     // 2: test // parameter
+
+//     console.log(index + ': ' + val);
+//   });
+
+  console.log(process.argv[1]);
+  console.log(process.argv[2]);
+
+  
